2ea840a74163758a019ef460e22353728bf85ed7
kkwcathy <kkwcathy@users.noreply.github.com>
2020-01-30 10:00:03 +0900

    Update 4_Boat.cpp

--- kkw-Algorithm/programmers-0129-greedy/4_Boat.cpp
+++ kkw-Algorithm/programmers-0129-greedy/4_Boat.cpp
@@ -1,85 +1,36 @@
-﻿// 오류 수정 중
-
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 
-
 using namespace std;
 
 int solution(vector<int> people, int limit) {
-	int answer = 0;
+	
+	int answer = 0; 
 
-	vector<int> saved;
+	sort(people.begin(), people.end()); // 몸무게를 오름차순으로 정렬
 
-	while (saved.size() < people.size())
+	int saved = 0; // 구조된 사람 수
+
+	vector<int>::iterator person1 = people.begin(); // 가장 가벼운 사람부터 시작
+	vector<int>::iterator person2 = people.end();	// 가장 무거운 사람부터 시작
+
+	while (saved < people.size()) // people의 수 만큼 모두 구조되면 종료
 	{
-		vector<int> left;
+		// 무거운 사람은 턴마다 무조건 구조
+		//(people.end()는 끝+1의 위치를 가리키는 포인터이므로 처음 시작할때 한번 빼주는 것도 있음)
+		person2--;
+		++saved;
 
-		for (int i : people) 
+		// 가벼운 사람의 몸무게와 무거운 사람의 몸무게 합이 limit보다 작으면 가벼운 사람도 구조
+		if (*person1 + *person2 <= limit) 
 		{
-			if (find(saved.begin(), saved.end(), i) != saved.end())
-			{
-				left.push_back(i);
-			}
+			++saved;
+			++person1;
 		}
 
-		int person1 = -1;
-		int person2 = -1;
-
-		int turn = 0;
-
-		/*for (int i = 0; i < people.size(); i++) 
-		{
-			if (find(saved.begin(), saved.end(), i) != saved.end()) continue;
-
-			person1 = i;
-
-			for (int j = 0; j < people.size(); j++) 
-			{
-				if (i == j || find(saved.begin(), saved.end(), j) != saved.end()) continue;
-
-				int total = people[i] + people[j];
-
-				if (total > turn && total <= limit) 
-				{
-					person2 = j;
-					turn = total;
-				}
-			}
-		}
-*/
-
-		for (int i = 0; i < left.size() - 1; i++)
-		{
-			person1 = i;
-			person2 = i + 1;
-		
-			int total = left[i] + left[i+1];
-
-			if (total > turn && total <= limit)
-			{
-				turn = total;
-				person1 = i;
-				person2 = i + 1;
-			}
-
-		}
-
-		left.clear();
-
-		saved.push_back(person1);
-
-		if (person2 >= 0) 
-		{
-			saved.push_back(person2);
-		}
-
-		cout << "1= " << person1 << "  2= " << person2 << endl;
-
-		++answer;
-
+		++answer; // 턴 더해주기
 	}
 
 	return answer;
