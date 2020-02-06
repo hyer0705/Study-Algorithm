adc2267ac13985f614203313a28d841dafcc03a5
HeejunShon <53172999+HeejunShon@users.noreply.github.com>
2020-02-06 15:17:07 +0900

    2/6

--- HJ-Algorithm/programmers/hash/ListOfNumber.java
+++ HJ-Algorithm/programmers/hash/ListOfNumber.java
@@ -1,14 +1,19 @@
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class ListOfNumber {
     public static boolean solution(String[] phone_book) {
-        String prefix = phone_book[0];
         
-        for(int i = 1; i < phone_book.length; i++){
-        	if(prefix.length() > phone_book[i].length())
-        		continue;
-        	else if(prefix.equals(phone_book[i].substring(0, prefix.length())))
-        		return false;
+        //정렬하기
+    	Arrays.sort(phone_book);
+    	
+        //정렬후 i, j = i + 1 비교
+        for(int i = 0; i < phone_book.length - 1; i++){
+        	for(int j = i + 1; j < phone_book.length; j++){
+        		if(phone_book[i].length() > phone_book[j].length())
+        			continue;
+        		else if(phone_book[i].equals(phone_book[j].substring(0, phone_book[i].length())))
+        			return false;        		
+        	}
         }
         
         return true;
@@ -16,7 +21,7 @@ public class ListOfNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//8,9번, 효율성 테스트 실패
+		//Solved
 
 	}
 
